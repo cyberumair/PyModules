a3c193e8746fba661fd088aad7691ddb83245876
cyberumair <umairshakoor.pro@gmail.com>
2025-08-14 09:57:13 +0500

    Added 3 modules GUI_input, Pkg_Installer & My Custom msg

---
+++ main_msg.py
@@ -0,0 +1,5 @@
+msg = "Thanks for using this PyModule, it was created by 'Umair Shakoor 😈' (@cyberUmair). Check other useful modules at 'https://github.com/cyberumair/PyModules' \n\tHappy Coding 🙂"
+
+if __name__ == '__main__':
+    print(f'\n{msg}')
+    print("\nThis message is used in every PyModule created by 'Umair Shakoor'\nIf you change this then it will update every where...\n")
